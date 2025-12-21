409454185a71e195e24c5b68b1074fe4cc30c126
shiv <shiv>
2025-12-21 14:01:53 +0000

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
+import PasswordForm from "./StarWars/PasswordForm";
+import MazeIntroPage from "./MazeGame/MazeIntroPage";
 import MazeGame from "./MazeGame/MazeGame";
 
 function App() {
   const [level, setLevel] = useState(0);
 
   // const goToSoundsRecall = () => setLevel(9);
+  const goToMazeIntro = () => setLevel(1);
+  const goToMazeGame = () => setLevel(2);
   const goToKeyUnlock = () => setLevel(2);
   // const goToBomberman = () => setLevel(3);
   // const goToQuiz = () => setLevel(4);
@@ -12,9 +16,9 @@ function App() {
 
   return (
     <div style={{ textAlign: "center" }}>
-      {/* {level === 0 && <PasswordForm onUnlock={goToMazeIntro} />}
-      {level === 1 && <MazeIntroPage onContinue={goToMazeGame} />} */}
-      {level === 0 && <MazeGame onWin={goToKeyUnlock} />}
+      {level === 0 && <PasswordForm onUnlock={goToMazeIntro} />}
+      {level === 1 && <MazeIntroPage onContinue={goToMazeGame} />}
+      {level === 2 && <MazeGame onWin={goToKeyUnlock} />}
       {/* {level === 0 && <SoundsIntro onContinue={goToSoundsRecall}/>}
       {level === 9 && <SoundsRecall onWin={goToKeyUnlock} />} */}
       {/* {level === 0 && <MazeIntroPage onContinue={goToMazeGame} />}
